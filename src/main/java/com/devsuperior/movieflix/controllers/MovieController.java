1256a25f0b1d416d3fb9529fc0e2ef8200f8de8f
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2024-10-09 17:10:12 -0300

    Movie findById authenticated e validated

--- src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -1,7 +1,12 @@
 package com.devsuperior.movieflix.controllers;
 
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +16,11 @@ public class MovieController {
 
     @Autowired
     private MovieService service;
+
+    @PreAuthorize("hasAnyRole('ROLE_VISITOR', 'ROLE_MEMBER')")
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieDetailsDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
 
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.movieflix.services;
 
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
 import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +12,9 @@ public class MovieService {
     @Autowired
     private MovieRepository repository;
 
+    public MovieDetailsDTO findById(Long id) {
+        return repository.findById(id)
+                .map(MovieDetailsDTO::new)
+                .orElseThrow(() -> new ResourceNotFoundException("Filme não encontrado"));
+    }
 }
